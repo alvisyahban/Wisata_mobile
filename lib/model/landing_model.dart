d10bce87aef3db32debee819b8884f5bfebf22fc
alvisyahban217@gmail.com <fauzybudiman22@gmail>
2024-11-07 09:47:05 +0700

    Pertemuan5

---
+++ lib/model/landing_model.dart
@@ -0,0 +1,20 @@
+class LandingModel {
+  String image, name;
+
+  LandingModel({required this.image, required this.name});
+}
+
+List<LandingModel> landingpage = [
+  LandingModel(
+      image:
+          'https://images.unsplash.com/photo-1559981421-3e0c0d712e3b?q=80&w=1916&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name: 'Jelajahi Indonesia Bersama Kami'),
+  LandingModel(
+      image:
+          'https://images.unsplash.com/photo-1611457194403-d3aca4cf9d11?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name: 'Keindahan Alam Indonesia'),
+  LandingModel(
+      image:
+          'https://images.unsplash.com/photo-1590796583326-afd3bb20d22d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      name: 'Kesejukan Angin Pegunungan'),
+];
